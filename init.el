0d035251ed6d1f2bdb753cc0251c6544e1702d9f
Torjus Haukom <thaukom@cisco.com>
2023-06-05 16:07:20 +0200

    init.el: div changes

--- init.el
+++ init.el
@@ -33,9 +33,8 @@ There are two things you can do about this warning:
 (global-linum-mode 1)
 (tool-bar-mode -1)
 (setq column-number-mode t)
+(setq show-trailing-whitespace t)
 (electric-pair-mode t)
-(require 're-builder)
-(setq reb-re-syntax 'string)
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(fullscreen . fullheight))
 (setq-default indent-tabs-mode nil)
@@ -57,6 +56,10 @@ There are two things you can do about this warning:
   (package-install 'use-package))
 (require 'use-package)
 (setq use-package-verbose t)
+
+(use-package re-builder
+  :config
+  (setq reb-re-syntax 'string))
 
 ;; Theme
 (use-package tree-sitter
@@ -80,7 +83,7 @@ There are two things you can do about this warning:
   (global-set-key (kbd "C-c a") 'org-agenda)
   (global-set-key (kbd "C-c l") 'org-store-link)
   (setq org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
-(setq org-agenda-files '("~/Documents/org")))
+(setq org-agenda-files (directory-files-recursively "~/Documents/org/" "\\.org$")))
 
 ;; Magit
 (use-package magit
@@ -105,8 +108,9 @@ There are two things you can do about this warning:
 ;; Projectile
 (use-package projectile
   :ensure t
+  :config
+  (setq projectile-enable-caching t)
   :init
-  (setq projectile-keymap-prefix (kbd "s-p"))
   (projectile-mode))
 
 (use-package helm-projectile
@@ -134,12 +138,12 @@ There are two things you can do about this warning:
 (use-package multiple-cursors
   :ensure t
   :bind
-  (("C-S-c C-S-c" . mc/edit-lines)
+  (("C-c C-l" . mc/edit-lines)
    ("C->" . mc/mark-next-like-this)
    ("C-<" . mc/mark-previous-like-this)
-   ("C-c C-<" . mc/mark-all-like-this)
+   ("C-c C-a" . mc/mark-all-like-this)
    ("C-c DEL" . mc/skip-to-next-like-this)
-   ("s-mouse-1" . mc/add-cursor-on-click)))
+   ("s-<mouse-1>" . mc/add-cursor-on-click)))
 
 (use-package dumb-jump
   :ensure t
@@ -169,60 +173,71 @@ There are two things you can do about this warning:
   ;; (setq dired-sidebar-use-custom-font t))
 
 
-;;lsp
-(use-package lsp-mode
-  :commands lsp
-  :ensure t
-  :config
-  (setq lsp-diagnostic-package nil)
-  (setq lsp-enable-file-watchers nil))
+;; ;;lsp
+;; (use-package lsp-mode
+;;   :commands lsp
+;;   :ensure t
+;;   :config
+;;   (setq lsp-diagnostic-package nil)
+;;   (setq lsp-enable-file-watchers nil))
 
-(use-package lsp-ui
-  :commands lsp-ui-mode
-  :ensure t
-  :config
-  (setq lsp-lens-enable nil)
-  (setq lsp-headerline-breadcrumb-enable nil))
-;; (setq company-transformers nil company-lsp-async t company-lsp-cache-candidates nil)
+;; (use-package lsp-ui
+;;   :commands lsp-ui-mode
+;;   :ensure t
+;;   :config
+;;   (setq lsp-lens-enable nil)
+;;   (setq lsp-headerline-breadcrumb-enable nil))
+;; ;; (setq company-transformers nil company-lsp-async t company-lsp-cache-candidates nil)
 
-(use-package ccls
-  :ensure t
-  :config
-  (setq ccls-executable "ccls")
-  (setq ccls-args '("--log-file=/tmp/ccls-log.txt"))
-  ;; (setq lsp-prefer-flymake nil)
-  ;; (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
-  :hook ((c-mode c++-mode objc-mode) .
-         (lambda () (require 'ccls) (lsp))))
+;; (use-package ccls
+;;   :ensure t
+;;   :config
+;;   (setq ccls-executable "ccls")
+;;   (setq ccls-args '("--log-file=/tmp/ccls-log.txt"))
+;;   ;; (setq lsp-prefer-flymake nil)
+;;   ;; (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
+;;   :hook ((c-mode c++-mode objc-mode) .
+;;          (lambda () (require 'ccls) (lsp))))
 
 
 ;; elpy
 (use-package elpy
   :ensure t
-  :init
+  :config
   (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
   (elpy-enable))
 
+(use-package cc-mode
+  :config
+  (setq c-default-style "stroustrup"
+        c-basic-offset 4))
+
 ;; nxml
-(defun nxml-where ()
-      "Display the hierarchy of XML elements the point is on as a path."
-      (interactive)
-      (let ((path nil))
-        (save-excursion
-          (save-restriction
-            (widen)
-            (while (and (< (point-min) (point)) ;; Doesn't error if point is at beginning of buffer
-                        (condition-case nil
-                            (progn
-                              (nxml-backward-up-element) ; always returns nil
-                              t)
-                          (error nil)))
-              (setq path (cons (xmltok-tag-local-name) path)))
-            (if (called-interactively-p t)
-                (message "/%s" (mapconcat 'identity path "/"))
-              (format "/%s" (mapconcat 'identity path "/")))))))
+(use-package nxml-mode
+  ;; :ensure t
+  :config
+  (setq nxml-slash-auto-complete-flag t))
+
+;; (defun nxml-where ()
+;;   "Display the hierarchy of XML elements the point is on as a path."
+;;   (interactive)
+;;   (let ((path nil))
+;;     (save-excursion
+;;       (save-restriction
+;;         (widen)
+;;         (while (and (< (point-min) (point)) ;; Doesn't error if point is at beginning of buffer
+;;                     (condition-case nil
+;;                         (progn
+;;                           (nxml-backward-up-element) ; always returns nil
+;;                           t)
+;;                       (error nil)))
+;;           (setq path (cons (xmltok-tag-local-name) path)))
+;;         (if (called-interactively-p t)
+;;             (message "/%s" (mapconcat 'identity path "/"))
+;;           (format "/%s" (mapconcat 'identity path "/")))))))
+
 ;; Edit functions
 (defun select-line (arg)
   (interactive "p")
@@ -232,15 +247,19 @@ There are two things you can do about this warning:
   (forward-line arg))
 (global-set-key "\C-l" 'select-line)
 
-;;  Languages  ;;
-;; c
-(setq c-default-style "stroustrup"
-      c-basic-offset 4)
+;; Function list
+(let ((primitives-list '()))
+     (mapatoms
+      (lambda (sym)
+        (if (subrp (symbol-function sym))
+           (push sym primitives-list))))
+     primitives-list)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-export-backends '(ascii html icalendar latex md odt))
  '(package-selected-packages
-   '(buffer-move transpose-frame org-bullets flycheck company-rtags flycheck-rtags helm-rtags projectile helm-gtags magit helm arjen-grey-theme)))
+   '(tree-sitter-langs tree-sitter helm-xref dumb-jump buffer-move transpose-frame org-bullets flycheck company-rtags flycheck-rtags helm-rtags projectile helm-gtags magit helm arjen-grey-theme)))
